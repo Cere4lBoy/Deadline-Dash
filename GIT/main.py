0507d9cc9a123b2ddd92aa169ddc17d06a279a91
Cereal <imanthaqifn@gmail.com>
2024-04-25 11:56:02 +0800

    saving

--- GIT/main.py
+++ GIT/main.py
@@ -1,15 +1,20 @@
 import pygame
+import sys
 
 pygame.init()
 
-screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('Fist')
+screen = pygame.display.set_mode((1366, 768))
+
 
 clock = pygame.time.Clock()
 
 while True:
-    for event in pygame.event.get()
+    for event in pygame.event.get():
         if event.type ==pygame.QUIT:
             pygame.quit
             sys.exit()
+
+
     pygame.display.update()
     clock.tick(144)
