58840e8683aa529c3279be799db81764a165f9f6
Cereal <imanthaqifn@gmail.com>
2024-04-25 23:07:28 +0800

    oke

---
+++ Graphics/WORLD_DESIGN/W1_FLOOR.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="W1_FLOOR" tilewidth="32" tileheight="32" tilecount="1024" columns="256">
+ <image source="W1_FLOOR.png" width="8196" height="154"/>
+</tileset>
