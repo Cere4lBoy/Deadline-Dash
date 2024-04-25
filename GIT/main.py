858bf6d230dbd3a36dc94212f66ebc2a7363e993
Cereal <imanthaqifn@gmail.com>
2024-04-25 12:10:56 +0800

    Updated

--- GIT/main.py
+++ GIT/main.py
@@ -1,20 +1,26 @@
 import pygame
 import sys
 
-pygame.init()
 
-pygame.display.set_caption('Fist')
-screen = pygame.display.set_mode((640, 480))
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('Fist')
+        self.screen = pygame.display.set_mode((1366, 768))
+       
+        self.clock = pygame.time.Clock()
+
+        self.img = pygame.image.load('Graphics/MAIN MENU/PREVIEW.png')
+
+    def run(self):
+        while True:
+             for event in pygame.event.get():
+               if event.type ==pygame.QUIT:
+                    pygame.quit
+                    sys.exit()
 
 
-clock = pygame.time.Clock()
-
-while True:
-    for event in pygame.event.get():
-        if event.type ==pygame.QUIT:
-            pygame.quit
-            sys.exit()
-
-
-    pygame.display.update()
-    clock.tick(144)
+             pygame.display.update()
+             self.clock.tick(144)
+Game().run()
