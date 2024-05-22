5bb2102de1c180a01df0da8aaa3cfda5b1512ea7
Cereal <imanthaqifn@gmail.com>
2024-05-23 03:40:26 +0800

    60percentprogress

--- Game.py
+++ Game.py
@@ -1,10 +1,19 @@
 import sys
+import math
+import random
 import pygame
-from scripts.utils import load_image, load_images
-from scripts.entities import PhysicsEntity
+from scripts.utils import load_image, load_images, Animation
+from scripts.entities import Player, Enemy
 from scripts.tilemap import Tilemap
 from button import Button
 from scripts.clouds import Clouds
+from scripts.particle import Particle
+from scripts.spark import Spark
+
+def scale_image(image, scale_factor):
+    width = int(image.get_width() * scale_factor)
+    height = int(image.get_height() * scale_factor)
+    return pygame.transform.scale(image, (width, height))
 
 class Game:
     def __init__(self):
@@ -13,18 +22,12 @@ class Game:
         pygame.display.set_caption('Deadline Dash')
         self.screen = pygame.display.set_mode((1366, 768))
         self.display = pygame.Surface((320, 240))
+
         self.clock = pygame.time.Clock()
         self.movement = [False, False]
-        
+
         # Load menu assets and capture returned values
-        logo_img, logo_width = self.load_menu_assets()
-
-        self.player = PhysicsEntity(self, 'player', (50, 50), (15, 17))
-        self.tilemap = Tilemap(self, tile_size=16)
-        # Storing logo_img and logo_width
-        self.logo_img = logo_img
-        self.logo_width = logo_width
-
+        logo_img, logo_width, logo_height = self.load_menu_assets()
 
         self.assets = {
             'decor': load_images('tiles/decor'),
@@ -34,55 +37,68 @@ class Game:
             'player': load_image('entities/player.png'),
             'background': load_image('background.png'),
             'clouds': load_images('clouds'),
+            'enemy/idle': Animation([scale_image(img, 0.4) for img in load_images('entities/enemy/idle')], img_dur=6),
+            'enemy/run': Animation([scale_image(img, 0.4) for img in load_images('entities/enemy/run')], img_dur=4),
+            'player/idle': Animation(load_images('entities/player/idle'), img_dur=6),
+            'player/run': Animation(load_images('entities/player/run'), img_dur=4),
+            'player/jump': Animation(load_images('entities/player/jump')),
+            'player/slide': Animation(load_images('entities/player/slide')),
+            'player/wall_slide': Animation(load_images('entities/player/wall_slide')),
+            'particle/leaf': Animation(load_images('particles/leaf'), img_dur=20, loop=False),
+            'particle/particle': Animation(load_images('particles/particle'), img_dur=6, loop=False),
+            'gun': load_image('gun.png'),
+            'projectile': load_image('projectile.png'),
         }
-        # Camera     
-        self.scroll = [0,0]
 
-        # clouds
         self.clouds = Clouds(self.assets['clouds'], count=16)
+        self.player = Player(self, (50, 50), (8, 15))
+        self.tilemap = Tilemap(self, tile_size=16)
 
+        # Storing logo_img and logo dimensions
+        self.logo_img = logo_img
+        self.logo_width = logo_width
+        self.logo_height = logo_height
 
-        
+        self.load_level(0)
+
     def load_menu_assets(self):
         # B.L.O.A.T (Best Logo Of All Time)
         logo_img = pygame.image.load('data/images/DEADLINEDASHLOGO.png').convert_alpha()
-        logo_width = int(logo_img.get_width() * 1.0)  # Scale logo width to 50%
-        logo_height = int(logo_img.get_height() * 1.0)  # Scale logo height to 50%
+        logo_width = int(logo_img.get_width() * 0.3)
+        logo_height = int(logo_img.get_height() * 0.3)
         logo_img = pygame.transform.scale(logo_img, (logo_width, logo_height))
-
 
         # Load button images
         start_img = pygame.image.load('data/images/menubuttons/START.png').convert_alpha()
         exit_img = pygame.image.load('data/images/menubuttons/EXIT.png').convert_alpha()
         leaderb_img = pygame.image.load('data/images/menubuttons/LEADERBOARD.png').convert_alpha()
         credits_img = pygame.image.load('data/images/menubuttons/CREDITS.png').convert_alpha()
-        
 
         # Background Image
         self.background_img = pygame.image.load('data/images/MENUBG.png').convert()
 
-        
         # Button objects
-        self.start_button = Button(550, 450, start_img, 0.7)
-        self.exit_button = Button(700, 650, exit_img, 0.5)
-        self.leaderb_button = Button(550, 550, leaderb_img, 0.7)
-        self.credits_button = Button(470, 650, credits_img, 0.5)
+        self.start_button = Button(800, 300, start_img, 0.7)
+        self.exit_button = Button(800, 400, exit_img, 0.7)
+        self.leaderb_button = Button(800, 200, leaderb_img, 0.5)
+        self.credits_button = Button(800, 500, credits_img, 0.7)
 
-        return logo_img, logo_width
+        return logo_img, logo_width, logo_height
 
     def draw_menu(self):
         self.screen.blit(self.background_img, (0, 0))
-        self.screen.blit(self.logo_img, ((self.screen.get_width() - self.logo_width) // 2, 50))  # Adjust Y position as needed
+        self.screen.blit(self.logo_img, (50, (self.screen.get_height() - self.logo_height) // 2))  # Center logo vertically
 
         # Draw buttons and handle actions
         if self.start_button.draw(self.screen):
             self.start_game()
         if self.exit_button.draw(self.screen):
-            pygame.quit()  # Quit the game
+            pygame.quit()
+            sys.exit()
         if self.leaderb_button.draw(self.screen):
-            pygame.quit()
+            pass
         if self.credits_button.draw(self.screen):
-            pygame.quit()
+            pass
 
         pygame.display.update()
 
@@ -99,25 +115,103 @@ class Game:
                     sys.exit()
             pygame.display.update()
 
+    def load_level(self, map_id):
+        self.tilemap.load('data/maps/' + str(map_id) + '.json')
+
+        self.leaf_spawners = []
+        for tree in self.tilemap.extract([('large_decor', 2)], keep=True):
+            self.leaf_spawners.append(pygame.Rect(4 + tree['pos'][0], 4 + tree['pos'][1], 23, 13))
+
+        self.enemies = []
+        for spawner in self.tilemap.extract([('spawners', 0), ('spawners', 1)]):
+            if spawner['variant'] == 0:
+                self.player.pos = spawner['pos']
+            else:
+                self.enemies.append(Enemy(self, spawner['pos'], (8, 15)))
+
+        self.projectiles = []
+        self.particles = []
+        self.sparks = []
+
+        self.scroll = [0, 0]
+        self.dead = 0
+
+    def adjust_enemy_position(self, enemy_index, new_position):
+        if 0 <= enemy_index < len(self.enemies):
+            self.enemies[enemy_index].pos = new_position
+
     def run(self):
         self.run_menu()  # Run the menu
         while True:
             self.display.blit(self.assets['background'], (0, 0))
 
+            if self.dead:
+                self.dead += 1
+                if self.dead > 40:
+                    self.load_level(0)
+
             self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
 
-            # Clouds Rendering
+            for rect in self.leaf_spawners:
+                if random.random() * 49999 < rect.width * rect.height:
+                    pos = (rect.x + random.random() * rect.width, rect.y + random.random() * rect.height)
+                    self.particles.append(Particle(self, 'leaf', pos, velocity=[-0.1, 0.3], frame=random.randint(0, 20)))
+
             self.clouds.update()
             self.clouds.render(self.display, offset=render_scroll)
 
-            # Camera
             self.tilemap.render(self.display, offset=render_scroll)
 
-            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display, offset=render_scroll)
-            
+            for enemy in self.enemies.copy():
+                kill = enemy.update(self.tilemap, (0, 0))
+                enemy.render(self.display, offset=render_scroll)
+                if kill:
+                    self.enemies.remove(enemy)
+
+            if not self.dead:
+                self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+                self.player.render(self.display, offset=render_scroll)
+
+            # [[x, y], direction, timer]
+            for projectile in self.projectiles.copy():
+                projectile[0][0] += projectile[1]
+                projectile[2] += 1
+                img = self.assets['projectile']
+                self.display.blit(img, (projectile[0][0] - img.get_width() / 2 - render_scroll[0], projectile[0][1] - img.get_height() / 2 - render_scroll[1]))
+                if self.tilemap.solid_check(projectile[0]):
+                    self.projectiles.remove(projectile)
+                    for i in range(4):
+                        self.sparks.append(Spark(projectile[0], random.random() - 0.5 + (math.pi if projectile[1] > 0 else 0), 2 + random.random()))
+                elif projectile[2] > 360:
+                    self.projectiles.remove(projectile)
+                elif abs(self.player.dashing) < 50:
+                    if self.player.rect().collidepoint(projectile[0]):
+                        self.projectiles.remove(projectile)
+                        self.dead += 1
+                        for i in range(30):
+                            angle = random.random() * math.pi * 2
+                            speed = random.random() * 5
+                            self.sparks.append(Spark(self.player.rect().center, angle, 2 + random.random()))
+                            self.particles.append(Particle(self, 'particle', self.player.rect().center, velocity=[math.cos(angle + math.pi) * speed * 0.5, math.sin(angle + math
+
+.pi) * speed * 0.5], frame=random.randint(0, 7)))
+
+            for spark in self.sparks.copy():
+                kill = spark.update()
+                spark.render(self.display, offset=render_scroll)
+                if kill:
+                    self.sparks.remove(spark)
+
+            for particle in self.particles.copy():
+                kill = particle.update()
+                particle.render(self.display, offset=render_scroll)
+                if particle.type == 'leaf':
+                    particle.pos[0] += math.sin(particle.animation.frame * 0.035) * 0.3
+                if kill:
+                    self.particles.remove(particle)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -128,15 +222,27 @@ class Game:
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
                     if event.key == pygame.K_UP:
-                        self.player.velocity[1] = -3
+                        self.player.jump()
+                    if event.key == pygame.K_x:
+                        self.player.dash()
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
-            
+
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
 
 Game().run()
+
+# Example usage:
+# After loading the level
+game = Game()
+game.run()
+
+# Adjust enemy position dynamically
+new_position = (100, 150)  # New x, y coordinates for the enemy
+game.adjust_enemy_position(0, new_position)  # Adjust the position of the first enemy
+
---
+++ game - Copy.py
@@ -0,0 +1,267 @@
+import sys
+import math
+import random
+import pygame
+from scripts.utils import load_image, load_images, Animation
+from scripts.entities import Player, Enemy
+from scripts.tilemap import Tilemap
+from button import Button
+from scripts.clouds import Clouds
+from scripts.particle import Particle
+from scripts.spark import Spark
+from scripts.pyvidplayer import Video
+
+def scale_image(image, scale_factor):
+    width = int(image.get_width() * scale_factor)
+    height = int(image.get_height() * scale_factor)
+    return pygame.transform.scale(image, (width, height))
+
+class Intro:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('Deadline Dash')
+        self.screen = pygame.display.set_mode((1366, 768))
+        self.clock = pygame.time.Clock()
+
+        # Load the video
+        self.vid = Video('data/group7intro.mp4')
+        self.vid.set_size((1366, 768))
+
+    def run(self):
+        intro_finished = False
+        while not intro_finished:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            # Play the video
+            self.vid.draw(self.screen, (0, 0))
+            pygame.display.update()
+            if not self.vid.update():
+                intro_finished = True
+
+            self.clock.tick(30)
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('Deadline Dash')
+        self.screen = pygame.display.set_mode((1366, 768))
+        self.display = pygame.Surface((320, 240))
+
+        self.clock = pygame.time.Clock()
+        self.movement = [False, False]
+
+        # Load menu assets and capture returned values
+        logo_img, logo_width, logo_height = self.load_menu_assets()
+
+        self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
+            'player': load_image('entities/player.png'),
+            'background': load_image('background.png'),
+            'clouds': load_images('clouds'),
+            'enemy/idle': Animation([scale_image(img, 0.4) for img in load_images('entities/enemy/idle')], img_dur=6),
+            'enemy/run': Animation([scale_image(img, 0.4) for img in load_images('entities/enemy/run')], img_dur=4),
+            'player/idle': Animation(load_images('entities/player/idle'), img_dur=6),
+            'player/run': Animation(load_images('entities/player/run'), img_dur=4),
+            'player/jump': Animation(load_images('entities/player/jump')),
+            'player/slide': Animation(load_images('entities/player/slide')),
+            'player/wall_slide': Animation(load_images('entities/player/wall_slide')),
+            'particle/leaf': Animation(load_images('particles/leaf'), img_dur=20, loop=False),
+            'particle/particle': Animation(load_images('particles/particle'), img_dur=6, loop=False),
+            'gun': load_image('gun.png'),
+            'projectile': load_image('projectile.png'),
+        }
+
+        self.clouds = Clouds(self.assets['clouds'], count=16)
+        self.player = Player(self, (50, 50), (8, 15))
+        self.tilemap = Tilemap(self, tile_size=16)
+
+        # Storing logo_img and logo dimensions
+        self.logo_img = logo_img
+        self.logo_width = logo_width
+        self.logo_height = logo_height
+
+        self.load_level(0)
+
+    def load_menu_assets(self):
+        # B.L.O.A.T (Best Logo Of All Time)
+        logo_img = pygame.image.load('data/images/DEADLINEDASHLOGO.png').convert_alpha()
+        logo_width = int(logo_img.get_width() * 0.3)
+        logo_height = int(logo_img.get_height() * 0.3)
+        logo_img = pygame.transform.scale(logo_img, (logo_width, logo_height))
+
+        # Load button images
+        start_img = pygame.image.load('data/images/menubuttons/START.png').convert_alpha()
+        exit_img = pygame.image.load('data/images/menubuttons/EXIT.png').convert_alpha()
+        leaderb_img = pygame.image.load('data/images/menubuttons/LEADERBOARD.png').convert_alpha()
+        credits_img = pygame.image.load('data/images/menubuttons/CREDITS.png').convert_alpha()
+
+        # Background Image
+        self.background_img = pygame.image.load('data/images/MENUBG.png').convert()
+
+        # Button objects
+        self.start_button = Button(800, 300, start_img, 0.7)
+        self.exit_button = Button(800, 400, exit_img, 0.7)
+        self.leaderb_button = Button(800, 200, leaderb_img, 0.5)
+        self.credits_button = Button(800, 500, credits_img, 0.7)
+
+        return logo_img, logo_width, logo_height
+
+    def draw_menu(self):
+        self.screen.blit(self.background_img, (0, 0))
+        self.screen.blit(self.logo_img, (50, (self.screen.get_height() - self.logo_height) // 2))  # Center logo vertically
+
+        # Draw buttons and handle actions
+        if self.start_button.draw(self.screen):
+            self.start_game()
+        if self.exit_button.draw(self.screen):
+            pygame.quit()
+            sys.exit()
+        if self.leaderb_button.draw(self.screen):
+            pass
+        if self.credits_button.draw(self.screen):
+            pass
+
+        pygame.display.update()
+
+    def start_game(self):
+        self.menu_running = False
+
+    def run_menu(self):
+        self.menu_running = True
+        while self.menu_running:
+            self.draw_menu()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            pygame.display.update()
+
+    def load_level(self, map_id):
+        self.tilemap.load('data/maps/' + str(map_id) + '.json')
+
+        self.leaf_spawners = []
+        for tree in self.tilemap.extract([('large_decor', 2)], keep=True):
+            self.leaf_spawners.append(pygame.Rect(4 + tree['pos'][0], 4 + tree['pos'][1], 23, 13))
+
+        self.enemies = []
+        for spawner in self.tilemap.extract([('spawners', 0), ('spawners', 1)]):
+            if spawner['variant'] == 0:
+                    self.player.pos = spawner['pos']
+            else:
+                self.enemies.append(Enemy(self, spawner['pos'], (8, 15)))
+
+        self.projectiles = []
+        self.particles = []
+        self.sparks = []
+
+        self.scroll = [0, 0]
+        self.dead = 0
+
+    def adjust_enemy_position(self, enemy_index, new_position):
+        if 0 <= enemy_index < len(self.enemies):
+            self.enemies[enemy_index].pos = new_position
+
+    def run(self):
+        self.run_menu()  # Run the menu
+        while True:
+            self.display.blit(self.assets['background'], (0, 0))
+
+            if self.dead:
+                self.dead += 1
+                if self.dead > 40:
+                    self.load_level(0)
+
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            for rect in self.leaf_spawners:
+                if random.random() * 49999 < rect.width * rect.height:
+                    pos = (rect.x + random.random() * rect.width, rect.y + random.random() * rect.height)
+                    self.particles.append(Particle(self, 'leaf', pos, velocity=[-0.1, 0.3], frame=random.randint(0, 20)))
+
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll)
+
+            self.tilemap.render(self.display, offset=render_scroll)
+
+            for enemy in self.enemies.copy():
+                kill = enemy.update(self.tilemap, (0, 0))
+                enemy.render(self.display, offset=render_scroll)
+                if kill:
+                    self.enemies.remove(enemy)
+
+            if not self.dead:
+                self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+                self.player.render(self.display, offset=render_scroll)
+
+            # [[x, y], direction, timer]
+            for projectile in self.projectiles.copy():
+                projectile[0][0] += projectile[1]
+                projectile[2] += 1
+                img = self.assets['projectile']
+                self.display.blit(img, (projectile[0][0] - img.get_width() / 2 - render_scroll[0], projectile[0][1] - img.get_height() / 2 - render_scroll[1]))
+                if self.tilemap.solid_check(projectile[0]):
+                    self.projectiles.remove(projectile)
+                    for i in range(4):
+                        self.sparks.append(Spark(projectile[0], random.random() - 0.5 + (math.pi if projectile[1] > 0 else 0), 2 + random.random()))
+                elif projectile[2] > 360:
+                    self.projectiles.remove(projectile)
+                elif abs(self.player.dashing) < 50:
+                    if self.player.rect().collidepoint(projectile[0]):
+                        self.projectiles.remove(projectile)
+                        self.dead += 1
+                        for i in range(30):
+                            angle = random.random() * math.pi * 2
+                            speed = random.random() * 5
+                            self.sparks.append(Spark(self.player.rect().center, angle, 2 + random.random()))
+                            self.particles.append(Particle(self, 'particle', self.player.rect().center, velocity=[math.cos(angle + math.pi) * speed * 0.5, math.sin(angle + math.pi) * speed * 0.5], frame=random.randint(0, 7)))
+
+            for spark in self.sparks.copy():
+                kill = spark.update()
+                spark.render(self.display, offset=render_scroll)
+                if kill:
+                    self.sparks.remove(spark)
+
+            for particle in self.particles.copy():
+                kill = particle.update()
+                particle.render(self.display, offset=render_scroll)
+                if particle.type == 'leaf':
+                    particle.pos[0] += math.sin(particle.animation.frame * 0.035) * 0.3
+                if kill:
+                    self.particles.remove(particle)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.player.jump()
+                    if event.key == pygame.K_x:
+                        self.player.dash()
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+                        
+
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+            pygame.display.update()
+            self.clock.tick(60)
+
+# Start the game by running the introduction
+if __name__ == "__main__":
+    intro = Intro()
+    intro.run()
